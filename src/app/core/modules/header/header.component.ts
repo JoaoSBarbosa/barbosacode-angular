e68337f7c0aea0d01db9bcb7de3c9fc62a7e5311
João <contato.jsbarbosa@gmail.com>
2025-10-27 22:20:48 -0300

    :sparkles: responsividade do menu

--- src/app/core/modules/header/header.component.ts
+++ src/app/core/modules/header/header.component.ts
@@ -3,39 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  menuOpen = false;
+
   links = [
-    {
-      id: 1,
-      label: 'Educação',
-      href: '#education',
-    },
-    {
-      id: 2,
-      label: 'Habilidades',
-      href: '#skill',
-    },
-    {
-      id: 3,
-      label: 'Experiência',
-      href: '#experience',
-    },
-    {
-      id: 4,
-      label: 'Tecnologias',
-      href: '#technologies',
-    },
-    {
-      id: 4,
-      label: 'Projetos',
-      href: '#projects',
-    },
-    {
-      id: 5,
-      label: 'Contato',
-      href: '#contact',
-    },
+    { id: 1, label: 'Educação', href: '#education' },
+    { id: 2, label: 'Habilidades', href: '#skill' },
+    { id: 3, label: 'Experiência', href: '#experience' },
+    { id: 4, label: 'Tecnologias', href: '#technologies' },
+    { id: 5, label: 'Projetos', href: '#projects' },
+    { id: 6, label: 'Contato', href: '#contact' },
   ];
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.style.overflow = this.menuOpen ? 'hidden' : 'auto';
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.style.overflow = 'auto';
+  }
 }
